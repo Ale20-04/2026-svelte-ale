5342864220fc69645ab597383b99fb23784cb7b2
alepolistena <alessandro.polistena@mail.polimi.it>
2026-04-14 22:04:22 +0200

    define and change state

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,18 @@
 <script>
-    console.log("Hello world");
+    let count = $state(0);
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<button on:click={() => count++ }>
+    clicks: {count}
+</button>
 
 <style>
     h1 {
         color: aqua;
     }
 
-p {
-    color: red;
-}
 </style>
 
