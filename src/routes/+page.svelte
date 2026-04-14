88ee7b7505f62ae0762c9ec14b0337c55164c709
alepolistena <alessandro.polistena@mail.polimi.it>
2026-04-14 21:41:28 +0200

    add script tag

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
+<script>
+    console.log("Hello world");
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<style>
+    h1 {
+        color: aqua;
+    }
+
+p {
+    color: red;
+}
+</style>
+
