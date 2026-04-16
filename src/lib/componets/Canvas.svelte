21fe287ef0831338a6cc33eda3a83088636a605a
alepolistena <alessandro.polistena@mail.polimi.it>
2026-04-16 20:18:52 +0200

    use effect to update canvas
    effect

--- src/lib/componets/Canvas.svelte
+++ src/lib/componets/Canvas.svelte
@@ -1,19 +1,18 @@
 <script>
-    import { onMount } from "svelte";
+    
 
     let size = $state(50)
     let color = $state("#ff3e00")
 
     let canvas 
 
-    onMount(() => {
+    $effect(() => {
          const context = canvas.getContext("2d")
          context.clearRect(0, 0, canvas.width, canvas.height)
     
          context.fillStyle = color
          context.fillRect(0, 0, size, size)
     })
-
 
 </script>
 
